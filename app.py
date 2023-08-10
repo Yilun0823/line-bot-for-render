e87f8ab23bcfb1dcf5b76270144ea996aa7d4828
楊宜倫 <87465975+Yilun0823@users.noreply.github.com>
2023-08-10 16:39:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ app = Flask(__name__)
 
 channel_serect=os.getenv("CHANNEL_SECRET")
 channel_access_token=os.getenv("CHANNEL_ACCESS_TOKEN")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 configuration = Configuration(access_token=channel_access_token)
 handler = WebhookHandler(channel_serect)
@@ -61,23 +62,24 @@ def callback():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessageContent)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    with ApiClient(configuration) as api_client:
-        # 當使用者傳入文字訊息時
-        print("使用者傳入文字訊息了！")
-        print(event)
-        line_bot_api = MessagingApi(api_client)
-        user_msg = event.message.text
-        bot_msg = TextMessage(text=f"Hello 你剛才說的是: {user_msg}")
-        
-        if user_msg in faq:
-            bot_msg = faq[user_msg]
-        elif user_msg.lower()in ["menu","選單","home","主選單"]:
-            bot_msg = menu
-        elif user_msg in table:
-            buy=table[user_msg]["buy"]
-            sell=table[user_msg]["sell"]
-            bot_msg = TextMessage(text=f"{user_msg}\n買價:{buy}\n賣價:{sell}")
+    user_msg = event.message.text
+
+    if user_msg.lower() in ["menu", "選單", "home", "主選單"]:
+        bot_msg = menu
+    elif user_msg in table:
+        buy = table[user_msg]["buy"]
+        sell = table[user_msg]["sell"]
+        bot_msg = TextSendMessage(text=f"{user_msg}\n買價:{buy}\n賣價:{sell}")
+    else:
+        # 使用 OpenAI API 進行文字生成
+        openai_response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=user_msg,
+            max_tokens=50
+        )
+        bot_msg = TextSendMessage(text=openai_response.choices[0].text)
 
 
         line_bot_api.reply_message_with_http_info(
