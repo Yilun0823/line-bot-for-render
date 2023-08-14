80845f300c006650af1fe33c4a0af158bf79d017
楊宜倫 <87465975+Yilun0823@users.noreply.github.com>
2023-08-15 03:09:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -63,32 +63,37 @@ def callback():
     print("#" * 40)
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    user_msg = event.message.text
+    with ApiClient(configuration) as api_client:
+        # 當使用者傳入文字訊息時
+        print("使用者傳入文字訊息了！")
+        print(event)
+        line_bot_api = MessagingApi(api_client)
+        user_msg = event.message.text
 
-    if user_msg in table:
-        buy = table[user_msg]["buy"]
-        sell = table[user_msg]["sell"]
-        bot_msg = TextMessage(text=f"{user_msg}\n買價:{buy}\n賣價:{sell}")
-    elif user_msg.lower() in ["menu", "選單", "home", "主選單"]:
-        bot_msg = menu
-    else:
-        openai_response = openai.Completion.create(
-            engine="davinci",
-            prompt=user_msg,
-            max_tokens=50
-        )
-        bot_msg = TextMessage(text=openai_response.choices[0].text)
+        if user_msg in table:
+            buy = table[user_msg]["buy"]
+            sell = table[user_msg]["sell"]
+            bot_msg = TextMessage(text=f"{user_msg}\n買價:{buy}\n賣價:{sell}")
+        elif user_msg.lower() in ["menu", "選單", "home", "主選單"]:
+            bot_msg = menu
+        else:
+            openai_response = openai.Completion.create(
+                engine="davinci",
+                prompt=user_msg,
+                max_tokens=50
+            )
+            bot_msg = TextMessage(text=openai_response.choices[0].text)
 
-    line_bot_api.reply_message_with_http_info(
-        ReplyMessageRequest(
-            reply_token=event.reply_token,
-            messages=[
-                bot_msg
-            ]
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[
+                    bot_msg
+                ]
+            )
         )
-    )
 @handler.add(MessageEvent, message=StickerMessageContent)
 def handle_sticker_message(event):
     with ApiClient(configuration) as api_client:
