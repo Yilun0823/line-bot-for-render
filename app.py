d555beb3e264e60eb2e31258806ec31ef51c7b58
楊宜倫 <87465975+Yilun0823@users.noreply.github.com>
2023-08-15 02:06:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
 from flask import Flask, request, abort
-from linebot import (
-    LineBotApi,
+from linebot.v3 import (
     WebhookHandler
 )
-from linebot.exceptions import (
+from linebot.v3.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent,
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
     TextMessage,
-    TextSendMessage,
     StickerMessage,
     LocationMessage,
 )
-
-from modules.currency import get_exchange_table
-
-import os
 import openai
+import os
 
-# 請確保您的 OpenAI API 金鑰正確設定在環境變數 OPENAI_API_KEY 中
+# 請確保已經設定好 OPENAI_API_KEY
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def get_exchange_table():
+    # 在這裡實現您的取得匯率資料的邏輯
+    # 返回匯率資料表格
+    exchange_table = {
+        "USD": {"buy": 28.5, "sell": 28.8},
+        "JPY": {"buy": 0.26, "sell": 0.28},
+        # ...
+    }
+    return exchange_table
 
 table = get_exchange_table()
 print("table", table)
@@ -30,52 +38,95 @@ app = Flask(__name__)
 channel_secret = os.getenv("CHANNEL_SECRET")
 channel_access_token = os.getenv("CHANNEL_ACCESS_TOKEN")
 
-line_bot_api = LineBotApi(channel_access_token)
+configuration = Configuration(access_token=channel_access_token)
 handler = WebhookHandler(channel_secret)
 
 @app.route("/", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-    print("#" * 40)
-    app.logger.info("Request body: " + body)
     
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        app.logger.info("無效的簽名。請檢查您的通道訪問權杖/通道秘密。")
         abort(400)
-    print("#" * 40)
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    user_msg = event.message.text
+    with ApiClient(configuration) as api_client:
+        user_msg = event.message.text
+        bot_msg = TextMessage(text=f"Hello 你剛才說的是: {user_msg}")
 
-    if user_msg.lower() in ["menu", "選單", "home", "主選單"]:
-        bot_msg = TextSendMessage(text="這是選單的內容。")  # 修改成您想要的選單內容
-    elif user_msg in table:
-        buy = table[user_msg]["buy"]
-        sell = table[user_msg]["sell"]
-        bot_msg = TextSendMessage(text=f"{user_msg}\n買價:{buy}\n賣價:{sell}")
-    else:
-        openai_response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=user_msg,
-            max_tokens=50
+        if user_msg in table:
+            buy = table[user_msg]["buy"]
+            sell = table[user_msg]["sell"]
+            bot_msg = TextMessage(text=f"{user_msg}\n買價:{buy}\n賣價:{sell}")
+        elif user_msg.lower() in ["menu", "選單", "home", "主選單"]:
+            bot_msg = TextMessage(text="這是選單回應")
+        else:
+            # 使用 OpenAI API 進行對話生成
+            openai_response = openai.Completion.create(
+                engine="davinci",  # 使用適合的引擎
+                prompt=user_msg,
+                max_tokens=50
+            )
+            bot_msg = TextMessage(text=openai_response.choices[0].text)
+    
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[
+                    bot_msg
+                ]
+            )
         )
-        bot_msg = TextSendMessage(text=openai_response.choices[0].text)
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        bot_msg
-    )
+@handler.add(MessageEvent, message=StickerMessageContent)
+def handle_sticker_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        sticker_id = event.message.sticker_id
+        package_id = event.message.package_id
+        keywords = ", ".join(event.message.keywords)
+        
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[
+                    StickerMessage(package_id="6325", sticker_id="10979904"),
+                    TextMessage(text="You just sent a sticker. Here is the information of the sticker:"),
+                    TextMessage(text=f"package_id is {package_id}, sticker_id is {sticker_id}."),
+                    TextMessage(text=f"The keywords are {keywords}."),
+                ]
+            )
+        )
 
-# 下面是處理貼圖和位置訊息的函數，您可以根據需求進行修改，如果不需要可以保留空白
+@handler.add(MessageEvent, message=LocationMessageContent)
+def handle_location_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        latitude = event.message.latitude
+        longitude = event.message.longitude
+        address = event.message.address
+        
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[
+                    TextMessage(text="You just sent a location message."),
+                    TextMessage(text=f"The latitude is {latitude}."),
+                    TextMessage(text=f"The longitude is {longitude}."),
+                    TextMessage(text=f"The address is {address}."),
+                    LocationMessage(title="Here is the location you sent.", address=address, latitude=latitude, longitude=longitude)
+                ]
+            )
+        )
 
 if __name__ == "__main__":
-    print("[伺服器應用程式開始運行]")
+    print("[Server Application Starts]")
     port = int(os.environ.get('PORT', 5001))
-    print(f"[Flask即將運行於連接端口：{port}]")
-    print(f"若在本地測試，請執行命令以開啟測試隧道：./ngrok http {port}")
+    print(f"[Flask will run on port: {port}]")
+    print(f"If testing locally, run the following command to open a test tunnel: ./ngrok http {port}")
     app.run(host="0.0.0.0", port=port)
