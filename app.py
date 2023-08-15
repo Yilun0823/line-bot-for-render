c4af0455a39daec3ab434088e642833c38d3ec33
楊宜倫 <87465975+Yilun0823@users.noreply.github.com>
2023-08-15 09:58:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -48,6 +48,9 @@ configuration = None
 channel_secret = os.getenv("CHANNEL_SECRET")
 channel_access_token = os.getenv("CHANNEL_ACCESS_TOKEN")
 
+# 初始化 handler 對象
+handler = WebhookHandler(channel_serect)
+
 # 設置 OpenAI API 密鑰
 openai.api_key = "YOUR_OPENAI_API_KEY"
 
