617c93ea3a352144a310c4485632f9e53e936769
楊宜倫 <87465975+Yilun0823@users.noreply.github.com>
2023-08-15 10:38:28 +0800

    Create openai.py

---
+++ openai.py
@@ -0,0 +1,17 @@
+import openai
+
+# 设置你的 OpenAI GPT-3 密钥
+openai.api_key = "OPENAI_API_KEY"
+
+def generate_text(prompt):
+    response = openai.Completion.create(
+        engine="davinci",  # 使用 GPT-3.5 的 davinci 引擎
+        prompt=prompt,
+        max_tokens=50  # 生成的最大标记数
+    )
+    return response.choices[0].text.strip()
+
+if __name__ == "__main__":
+    prompt = "在未来的世界里，人类和机器将会"
+    generated_text = generate_text(prompt)
+    print("生成的文本：", generated_text)
