50c14f5b17f131d9f64f054071c2baa10d8f584c
楊宜倫 <87465975+Yilun0823@users.noreply.github.com>
2023-08-15 11:16:57 +0800

    Update openai.py

--- openai.py
+++ openai.py
@@ -1,18 +1,15 @@
-import openai
-
-# 設置你的 OpenAI GPT-3 密鑰
-openai.api_key = "OPENAI_API_KEY"
-
 def generate_text(prompt):
-    response = openai.Completion.create(
-        engine="davinci",  # 使用 GPT-3.5 的 davinci 引擎
-        prompt=prompt,
-        max_tokens=50  # 生成的最大標記數
-    )
-    print("OpenAI API 响应：", response)
-    return response.choices[0].text.strip()
-
-if __name__ == "__main__":
-    prompt = "在未來的世界里，人類和機器將會"
-    generated_text = generate_text(prompt)
-    print("生成的文本：", generated_text)
+    print("調用 OpenAI API 前...")
+    try:
+        response = openai.Completion.create(
+            engine="davinci",  # 使用 GPT-3.5 的 davinci 引擎
+            prompt=prompt,
+            max_tokens=50  # 生成的最大標記數
+        )
+        print("OpenAI API 響應：", response)
+        generated_text = response.choices[0].text.strip()
+        print("生成的文本：", generated_text)
+        return generated_text
+    except Exception as e:
+        print("調用 OpenAI API 時出現異常：", e)
+        return "抱歉，出了點問題，無法生成回答。"
